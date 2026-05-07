7e14e1c2229f382cd7d7e638ff7d8dd802e06238
Edward Fernando Perez Gonzalez <ia.pg101524@ufg.edu.sv>
2026-05-06 18:15:41 -0600

    Mejoras de seguridad

--- db.py
+++ db.py
@@ -1,10 +1,11 @@
+import os
 import mysql.connector
 
 def conectar():
     return mysql.connector.connect(
-        host="gateway01.us-east-1.prod.aws.tidbcloud.com",
-        port=4000,
-        user="3TGwmX9GJtH7Xoc.root",
-        password="qJYVUDm4T1Gzcx2J",  
-        database="inventario_ufg"
+        host=os.environ.get('DB_HOST'),
+        port=int(os.environ.get('DB_PORT', 4000)),
+        user=os.environ.get('DB_USER'),
+        password=os.environ.get('DB_PASSWORD'),
+        database=os.environ.get('DB_NAME')
     )
